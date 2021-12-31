5cf96ce2c208205893a37f942a8bf309a428b7b4
Reid <rlj0713@gmail.com>
2021-12-31 10:15:49 -0500

    fizz fuzz in python

--- practice.py
+++ practice.py
@@ -1,1 +1,12 @@
-# This is only a test
+
+def addMultiples(x, y, max_number):
+
+    total = 0
+
+    for i in range(max_number):
+        if i % x == 0 or i % y == 0:
+            total += i
+
+    print(total)
+
+addMultiples(3, 5, 1000)
